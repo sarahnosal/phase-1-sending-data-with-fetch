715466cf010dac30a42c0dc8804aca0c83daea4a
Sarah Nosal <nosalsm@gmail.com>
2021-11-30 14:59:41 -0600

    commit tree 43f4c6d90bcd04b7a421de72d1de161615e1dc84

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers:  {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+        },
+        body: JSON.stringify ({
+            name,
+            email
+        })
+    })
+    .then(resp => resp.json())
+    .then(json => {
+        console.log(json)
+        document.body.innerHTML = json['id']
+    })
+    .catch(error => {
+        alert('Bad things! Ragnarok!');
+        document.body.innerHTML = error.message;
+    })
+};
